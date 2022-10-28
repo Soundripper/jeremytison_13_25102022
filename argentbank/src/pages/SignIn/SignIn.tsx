4f1d5f778d3afb39a1b19f7f8a7e8967a8cb9dd0
Soundripper <jerrsoundripper@gmail.com>
2022-10-28 14:03:21 +0200

    npm install --save @types/react-dom

--- argentbank/src/pages/SignIn/SignIn.tsx
+++ argentbank/src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import SignInForm from "../../components/SignInForm/SignInForm";
 
 const SignIn = () => {
     return (
@@ -9,28 +9,7 @@ const SignIn = () => {
                 {/* <i className="fa fa-user-circle sign-in-icon"></i> */}
                 <FontAwesomeIcon icon={faUserCircle} className="uesrCircle"/>
                 <h1>Sign In</h1>
-
-                <form>
-                <div className="input-wrapper">
-                    <label htmlFor="username">Username</label
-                    ><input type="text" id="username" />
-                </div>
-                <div className="input-wrapper">
-                    <label htmlFor="password">Password</label
-                    ><input type="password" id="password" />
-                </div>
-                <div className="input-remember">
-                    <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
-                    >Remember me</label
-                    >
-                </div>
-                {/* PLACEHOLDER DUE TO STATIC SITE */}
-                <Link to="/user"  className="sign-in-button">Sign In</Link>
-                {/* <a href="./user.html" className="sign-in-button">Sign In</a> */}
-                {/* SHOULD BE THE BUTTON BELOW
-                <button className="sign-in-button">Sign In</button> */}
-                </form>
-                
+                <SignInForm />
             </section>
         </main>
     )
